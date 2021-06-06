cfe814f1079fb5225eca1a63d68ae66db073742c
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2021-06-06 23:09:15 +0530

    chapter 7: maps
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

---
+++ maps/dictionary.go
@@ -0,0 +1,62 @@
+package main
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Add append word and the definition to the dictionary
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Delete a word from the dictionary
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
+// Search returns the value of a given key from dictionary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// Update modifies definition of a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
